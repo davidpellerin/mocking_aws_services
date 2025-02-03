03546871f8a3570940f1d3fdebde0b71465daea1
David Pellerin <david@davidpellerin.com>
2025-02-03 17:33:46 -0500

    removing some logging

--- organizations.js
+++ organizations.js
@@ -6,17 +6,14 @@ import {
 async function createAccount(email, accountName, roleName, client = null) {
   try {
     if (client === null) {
-      console.log("Creating new client");
       client = new OrganizationsClient({});
     } else {
-      console.log("Using existing client");
     }
     const command = new CreateAccountCommand({
       Email: email,
       AccountName: accountName,
       RoleName: roleName,
     });
-    console.log("Sending command", command);
     const response = await client.send(command);
     return response;
   } catch (error) {
